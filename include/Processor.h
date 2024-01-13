e621af4d68da3d2286c608b0dab876dabb738f1e
SkyHorizon3 <neuner.r@gmx.de>
2024-01-13 23:51:34 +0100

    Update Processor.h

--- include/Processor.h
+++ include/Processor.h
@@ -15,7 +15,7 @@ public:
 private:
 
 	template <typename T>
-	void SetConstStrings(RE::TESForm* Form, RE::BSFixedString newName, RE::BSFixedString T::* memberPtr);
+	void SetConstStrings(RE::TESForm* Form, RE::BSFixedString NewString, RE::BSFixedString T::* memberPtr);
 
 	Processor() = default;
 	~Processor() = default;
