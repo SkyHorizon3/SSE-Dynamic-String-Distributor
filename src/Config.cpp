0fda31e3bc09c1a38d1c93e089a1a4e3cfccf905
SkyHorizon3 <neuner.r@gmx.de>
2024-01-27 14:50:58 +0100

    fix: fix directory iterator

--- src/Config.cpp
+++ src/Config.cpp
@@ -105,7 +105,7 @@ void Config::EnumerateFolder() //Get all folders in DynamicStringDistributor dir
 	const std::filesystem::path Directory = L"Data\\SKSE\\Plugins\\DynamicStringDistributor\\";
 	bool foundOverwriteFolder = false;  // To check if Overwrite folder is found
 
-	for (const auto& entry : std::filesystem::recursive_directory_iterator(Directory))
+	for (const auto& entry : std::filesystem::directory_iterator(Directory))
 	{
 		if (entry.is_directory())
 		{
