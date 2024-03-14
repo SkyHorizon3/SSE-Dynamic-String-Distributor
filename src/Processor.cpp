62ff02aa9b3c56b70b69111127584db7ae7f8c32
SkyHorizon3 <neuner.r@gmx.de>
2024-03-14 18:04:23 +0100

    fix: fix crash related to GameSettings

--- src/Processor.cpp
+++ src/Processor.cpp
@@ -60,7 +60,14 @@ void Processor::SetGameSettingString(const std::string& a_name, const std::strin
 {
 	auto setting = RE::GameSettingCollection::GetSingleton()->GetSetting(a_name.c_str());
 
-	//SKSE::log::info("String: {}", std::string(setting->data.s));
+	if (setting == nullptr)
+	{
+#ifndef NDEBUG
+
+		SKSE::log::debug("Faild to set GameSetting string for {}. NOTE: It is normal for this to happen with some settings, they are simply not loaded.", a_name.c_str());
+#endif
+		return;
+	}
 
 	if (setting->GetType() == RE::Setting::Type::kString)
 	{
